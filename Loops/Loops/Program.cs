428c15644782b60f3ff14506be8097209a2d0ad9
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-05-17 15:59:53 -0700

    updates

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1-15?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            do
+            {
+                switch (number)
+                {
+                    case 12:
+                        Console.WriteLine("You guessed the number 12. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong. You guessed " + number + " and that is incorrect.");
+                        Console.WriteLine("\nGuess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+            Console.Read();
+        }
+    }
+}
