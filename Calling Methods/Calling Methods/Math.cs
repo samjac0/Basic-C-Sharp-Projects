82e5d0584351fdf9e78bd35e2e59e3288a99a610
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-05-23 14:25:55 -0700

    updates

---
+++ Calling Methods/Calling Methods/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public class Math
+    {
+        public int multiplication(int userNum1)
+        {
+            return userNum1 * 2;
+        }
+
+        public int add(int userNum1)
+        {
+            return userNum1 + 6;
+        }
+
+        public int sub(int userNum1)
+        {
+            return userNum1 - 6;
+        }
+    }
+}
---
+++ TwentyOne(game)/TwentyOne(game)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne_game_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            deck = Shuffle(deck);
+            
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+            //deck.Cards = new List<Card>();
+
+
+            //Card cardOne = new Card();
+            //cardOne.Face = "Queen";
+            //cardOne.Suit = "Spades";
+
+            //deck.Cards.Add(cardOne);
+
+        }
+        public static Deck Shuffle(Deck deck)
+        {
+            List<Card> Templist = new List<Card>();
+            Random random = new Random();
+
+            while (deck.Cards.Count > 0)
+            {
+                int randomIndex = random.Next(0, deck.Cards.Count);
+                Templist.Add(deck.Cards[randomIndex]);
+                deck.Cards.RemoveAt(randomIndex);
+            }
+            deck.Cards = Templist;
+            return deck;
+
+        }
+    }
+}
