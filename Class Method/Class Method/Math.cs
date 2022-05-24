7243b9f953726b7dc673d117f7796edebe1ca4f7
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-05-24 14:46:54 -0700

    updates

---
+++ Class Method/Class Method/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    public class Math
+    {
+        //Divide Method
+        public static void div(int userNum1)
+        {
+            Console.WriteLine(userNum1 / 2);
+        }
+       
+        // Overload Methods
+        public int maths(int userNum1)
+        {
+            int sum = userNum1 + 2;
+            return sum;
+        }
+        // Overload Methods
+        public int maths(int userNum1, int userNum2)
+        {
+            int sum = userNum1 + 2 * userNum2;
+            return sum;
+        }
+        
+        // Out Method
+        public void output(out int userNum1)
+        {
+            userNum1 = 2;
+        }
+    }
+}
---
+++ Class Method/Class Method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this fun little program featuring Methods!\nEnter a number to get started.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nWant to add another number to the mix? If not, leave blank and press enter.");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\nHere's your first number below. It was divided by 2 using a static class method from 'Math.cs':");
+
+            Math.div(userNum1);
+
+            Console.WriteLine("\nHere's and example of an Overload Method");
+            Math obj1 = new Math();
+            Console.WriteLine(obj1.maths(userNum1));
+            Console.WriteLine(obj1.maths(userNum1, userNum2));
+            
+            Console.WriteLine("\nHere's an example of an Out method");
+            obj1.output(out userNum1);
+            Console.WriteLine(userNum1);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
