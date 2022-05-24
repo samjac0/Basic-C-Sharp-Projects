5ce42c1a4f4e8d44174636479991ed31193ca037
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-05-23 19:01:17 -0700

    updates

---
+++ Calling Methods - 4/Calling Methods/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    public class Math
+    {
+        public void add(int userNum1, int userNum2)
+        {
+            userNum1 += 2;
+            Console.WriteLine("\n" + userNum2);
+
+        }
+    }
+}
---
+++ Calling Methods - 4/Calling Methods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to this fun little program featuring Methods!\nEnter a number to get started.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nNow, write one additional number");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+            Math adding = new Math(); //Creates a new multiplication object to be imported from Math.cs
+            adding.add(userNum1, userNum2); //assigns object to 'result1'
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
