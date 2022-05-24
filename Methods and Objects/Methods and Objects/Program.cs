da3186e604b898640fa1421a93b0750b8985796b
samjac0 <98563031+samjac0@users.noreply.github.com>
2022-05-24 15:39:47 -0700

    updates

---
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person employee = new Person();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
